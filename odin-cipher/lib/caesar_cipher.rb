f827e08cc4f4cc76d1989b8c97cbaebb1646cf28
Zachary J Ricker <109747756+Clovena@users.noreply.github.com>
2025-03-22 22:01:42 -0400

    Fix upcase typo.

--- odin-cipher/lib/caesar_cipher.rb
+++ odin-cipher/lib/caesar_cipher.rb
@@ -4,7 +4,7 @@ def encrypt(str, shift = 0)
   end
   out = ''
   str.split('').each do |char|
-    base = char == char.upase ? 65 : 97
+    base = char == char.upcase ? 65 : 97
     num = char.ord
     if (num.between?(65, 90)) || (num.between?(97, 122))
       out += ((((num - base) + shift.to_i) % 26) + base).chr
