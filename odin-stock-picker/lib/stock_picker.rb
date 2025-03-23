d632be14a65d4dfb92fda435fa4b97371c933866
Zachary J Ricker <109747756+Clovena@users.noreply.github.com>
2025-03-23 17:06:12 -0400

    Init stock picker project. Complete program. WIP: testing.

---
+++ odin-stock-picker/lib/stock_picker.rb
@@ -0,0 +1,19 @@
+def stock_picker(arr)
+  return [0, 0] if ((arr.length < 2) || arr.class != Array)
+
+  gains = []
+  arr.each_with_index do |buy, buy_index|
+    day_gains = []
+    arr.each_with_index do |sell, sell_index|
+      day_gains << (sell_index > buy_index ? sell - buy : 0)
+    end
+    gains << day_gains
+  end
+
+  maxes = []
+  gains.each { |row| maxes << row.max }
+  max_gain = maxes.max
+  buy = maxes.index(max_gain)
+  buy_sell = [ buy, gains[buy].index(max_gain) ]
+  buy_sell
+end
