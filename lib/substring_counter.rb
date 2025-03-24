450c407163f324fce1495be0d80fc99f49d68f10
Zachary J Ricker <109747756+Clovena@users.noreply.github.com>
2025-03-24 12:08:52 -0400

    Add rejection line if dict is not Array. Write out full accumulator var name. Hotfix process of counting occurrences of substr in str. Use #select, not #compact, to remove absent substrs from result.

--- lib/substring_counter.rb
+++ lib/substring_counter.rb
@@ -1,8 +1,9 @@
 def substrings(str, dict = [])
+  return {} unless dict.class == Array
   str_clean = str.to_s.downcase
-  dict.reduce(Hash.new(0)) do |acc, substr|
+  dict.reduce(Hash.new(0)) do |accumulator, substr|
     substr_clean = substr.to_s.downcase
-    acc[substr_clean] += 1 if str_clean.include?(substr_clean)
-    acc.compact
+    accumulator[substr_clean] = str_clean.scan(/#{substr_clean}/).length
+    accumulator.select { |k, v| v > 0 }
   end
 end
