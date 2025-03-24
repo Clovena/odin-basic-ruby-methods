67cf58d005911087d1411a3ad03b7f8ef6400d6e
Zachary J Ricker <109747756+Clovena@users.noreply.github.com>
2025-03-24 11:41:52 -0400

    Fix typo in substring counter.

--- lib/substring_counter.rb
+++ lib/substring_counter.rb
@@ -1,7 +1,7 @@
 def substrings(str, dict = [])
   str_clean = str.to_s.downcase
   dict.reduce(Hash.new(0)) do |acc, substr|
-    substr_clean = subst.to_s.downcase
+    substr_clean = substr.to_s.downcase
     acc[substr_clean] += 1 if str_clean.include?(substr_clean)
     acc.compact
   end
