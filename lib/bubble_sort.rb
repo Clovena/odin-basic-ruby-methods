33d2067f045267dd21df9c2f40d1201a3cdb7acb
Zachary J Ricker <109747756+Clovena@users.noreply.github.com>
2025-03-26 18:56:16 -0400

    Refactor bubble sort based on rubocop feedback.

--- lib/bubble_sort.rb
+++ lib/bubble_sort.rb
@@ -7,11 +7,10 @@ def bubble_sort(arr = [])
   iterations = arr.length - 1
   iterations.times do |iteration|
     arr.each_index do |index|
-      unless index >= (iterations - iteration)
-        if arr[index] > arr[index + 1]
-          arr[index], arr[index + 1] = arr[index + 1], arr[index]
-        end
-      end
+      next if index >= (iterations - iteration)
+      next if arr[index] < arr[index + 1]
+
+      arr[index], arr[index + 1] = arr[index + 1], arr[index]
     end
   end
   arr
